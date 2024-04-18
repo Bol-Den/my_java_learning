c4b2ff7ccdc7825a260ccf70128901af825c4ce1
Den <deniss.boltunovs@gmail.com>
2024-04-18 20:31:35 +0300

    adjusted :)

--- src/Hello_01/HelloDeniss.java
+++ src/Hello_01/HelloDeniss.java
@@ -1,11 +1,7 @@
 package Hello_01;
 
 public class HelloDeniss {
-
     public static void main(String[] args) {
-
-        System.out.println("Hello Deniss :)");
-
+        System.out.println("Hello Deniss");
     }
-
 }
