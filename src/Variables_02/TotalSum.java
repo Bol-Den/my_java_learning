ece92eea903579eabef7d8ef48d35ad70cac9524
Den <deniss.boltunovs@gmail.com>
2024-04-19 13:49:16 +0300

    TotalSum class added

---
+++ src/Variables_02/TotalSum.java
@@ -0,0 +1,13 @@
+package Variables_02;
+
+public class TotalSum {
+    public static void main(String[] args) {
+
+        int FirstValue = 25;
+        int SecondValue = 11;
+        int Result = FirstValue + SecondValue ;
+
+        System.out.println(Result);
+
+    }
+}
